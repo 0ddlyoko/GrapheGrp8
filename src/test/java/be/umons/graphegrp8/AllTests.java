1d449fa346e2a41a1052343108eff9ed626e0c1b
0ddlyoko <0ddlyokoOfficial@gmail.com>
2019-11-26 23:57:36 +0100

    Add file AllTests to run all tests at one

---
+++ src/test/java/be/umons/graphegrp8/AllTests.java
@@ -0,0 +1,11 @@
+package be.umons.graphegrp8;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ NodeTest.class, NodeManagerTest.class })
+public class AllTests {
+
+}
